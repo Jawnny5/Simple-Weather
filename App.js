174d1625ab0fb655d4cfb476c8031363c6a6e5c6
“Jawnny5” <william.neal83@gmail.com>
2021-06-13 18:34:06 -0600

    location permission function

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location'
+import { LocationEventEmitter } from 'expo-location/build/LocationEventEmitter';
+
+const WEATHER_KEY = 'ac1015366220c6a0a90c676bca30c91e'
 
 export default function App() {
+
+  const [errorMsg, setErrorMsg] = useState(null)
+
+  useEffect(() => {
+    load()
+  }, [])
+
+  async function load(){
+    try {
+      let { status } = await Location.requestPermissionsAsync()
+
+      if(status !== 'granted'){
+        setErrorMsg('Access To Location Needed To Continue')
+        return
+      }
+      const location = await Location.getCurrentPositionAsync()
+      const { latitude, longitude} = location.coords
+      alert(`Latitude: ${latitude}, Longitude: ${longitude}`) 
+    } catch (error) {
+
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Bam First Mobile Edit!</Text>
       <StatusBar style="auto" />
     </View>
   );
